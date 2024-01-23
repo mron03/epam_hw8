6a14951f454ea5bee6e8a592aead84597f9ae336
mron03 <orazkhanmansur@gmail.com>
2024-01-23 22:33:09 +0600

    first commit

--- inference/run.py
+++ inference/run.py
@@ -13,14 +13,21 @@ from datetime import datetime
 from typing import List
 
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+
 
 # Adds the root directory to system path
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(ROOT_DIR))
 
 # Change to CONF_FILE = "settings.json" if you have problems with env variables
-CONF_FILE = os.getenv('CONF_PATH')
+CONF_FILE = "settings.json" 
 
 from utils import get_project_dir, configure_logging
 
@@ -41,6 +48,22 @@ parser.add_argument("--infer_file",
 parser.add_argument("--out_path", 
                     help="Specify the path to the output table")
 
+class IrisNet(nn.Module):
+
+    def __init__(self) -> None:
+        super(IrisNet, self).__init__()
+
+        self.fc1 = nn.Linear(4, 16)
+        self.fc2 = nn.Linear(16, 16)
+        self.fc3 = nn.Linear(16, 3)
+    
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+    
 
 def get_latest_model_path() -> str:
     """Gets the path of the latest saved model"""
@@ -53,13 +76,14 @@ def get_latest_model_path() -> str:
     return os.path.join(MODEL_DIR, latest)
 
 
-def get_model_by_path(path: str) -> DecisionTreeClassifier:
+def get_model_by_path(path: str) -> IrisNet:
     """Loads and returns the specified model"""
+    model = IrisNet()
     try:
-        with open(path, 'rb') as f:
-            model = pickle.load(f)
-            logging.info(f'Path of the model: {path}')
-            return model
+        model.load_state_dict(torch.load(path))
+        model.eval()
+        logging.info(f'Path of the model {path}')
+        return model
     except Exception as e:
         logging.error(f'An error occurred while loading the model: {e}')
         sys.exit(1)
@@ -75,11 +99,63 @@ def get_inference_data(path: str) -> pd.DataFrame:
         sys.exit(1)
 
 
-def predict_results(model: DecisionTreeClassifier, infer_data: pd.DataFrame) -> pd.DataFrame:
+def predict_results(model, infer_data_loader: DataLoader) -> pd.DataFrame:
     """Predict de results and join it with the infer_data"""
-    results = model.predict(infer_data)
-    infer_data['results'] = results
-    return infer_data
+    predictions = []
+    with torch.no_grad():
+        for batch in infer_data_loader:
+            inputs = batch[0]
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs, 1)
+            predictions.extend(predicted.numpy())
+    
+    with open('inference/inference_encoder.pkl', 'rb') as f:
+        encoder = pickle.load(f)
+
+    class_names = encoder.inverse_transform(predictions)
+
+    return class_names
+
+def process_data(infer_data: pd.DataFrame):
+
+    scaled_df, target = scale_label(infer_data)
+
+    X_tensor, y_tensor = split_tensor(scaled_df, target)
+
+    dataset_loader = load_dataset(X_tensor, y_tensor)
+
+    return dataset_loader
+
+
+def scale_label(df):
+    logging.info("Scaling and Labeling Data...")
+
+    features_scaled = df.drop(columns='Species')
+    scaled_df = StandardScaler().fit_transform(features_scaled)
+
+    encoder = LabelEncoder()
+    target = encoder.fit_transform(df['Species'])
+
+    # Save the fitted LabelBinarizer for later use
+    save_label_encoder(encoder)
+
+    return scaled_df, target
+
+def save_label_encoder(encoder):
+    with open('inference/inference_encoder.pkl', 'wb') as f:
+        pickle.dump(encoder, f)
+
+def split_tensor( X, y):    
+    logging.info("Converting Sets into Tensors...")
+    X_tensor = torch.tensor(X, dtype=torch.float32)
+    y_tensor = torch.tensor(y, dtype=torch.long)
+    return X_tensor, y_tensor
+
+def load_dataset(X_tensor, y_tensor,):
+    logging.info("Loading Data into Dataloader...")
+    dataset = TensorDataset(X_tensor, y_tensor)
+    dataset_loader = DataLoader(dataset, batch_size=conf['train']['batch_size'])
+    return dataset_loader
 
 
 def store_results(results: pd.DataFrame, path: str = None) -> None:
@@ -101,7 +177,9 @@ def main():
     model = get_model_by_path(get_latest_model_path())
     infer_file = args.infer_file
     infer_data = get_inference_data(os.path.join(DATA_DIR, infer_file))
-    results = predict_results(model, infer_data)
+    infer_data_loader = process_data(infer_data)
+    results = predict_results(model, infer_data_loader)
+
     store_results(results, args.out_path)
 
     logging.info(f'Prediction results: {results}')
--- training/train.py
+++ training/train.py
@@ -13,7 +13,13 @@ import time
 from datetime import datetime
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, classification_report, confusion_matrix
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
 
 # Comment this lines if you have problems with MLFlow installation
 import mlflow
@@ -24,7 +30,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(ROOT_DIR))
 
 # Change to CONF_FILE = "settings.json" if you have problems with env variables
-CONF_FILE = os.getenv('CONF_PATH') 
+CONF_FILE = "settings.json"
 
 from utils import get_project_dir, configure_logging
 
@@ -53,53 +59,138 @@ class DataProcessor():
     def prepare_data(self, max_rows: int = None) -> pd.DataFrame:
         logging.info("Preparing data for training...")
         df = self.data_extraction(TRAIN_PATH)
-        df = self.data_rand_sampling(df, max_rows)
-        return df
+
+        X, y = self.scale_label(df)
+
+        X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor = self.split_tensor(X, y)
+
+        train_loader, test_loader = self.dataset_loader(X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor)
+
+        return train_loader, test_loader
 
     def data_extraction(self, path: str) -> pd.DataFrame:
         logging.info(f"Loading data from {path}...")
         return pd.read_csv(path)
     
-    def data_rand_sampling(self, df: pd.DataFrame, max_rows: int) -> pd.DataFrame:
-        if not max_rows or max_rows < 0:
-            logging.info('Max_rows not defined. Skipping sampling.')
-        elif len(df) < max_rows:
-            logging.info('Size of dataframe is less than max_rows. Skipping sampling.')
-        else:
-            df = df.sample(n=max_rows, replace=False, random_state=conf['general']['random_state'])
-            logging.info(f'Random sampling performed. Sample size: {max_rows}')
-        return df
+    def scale_label(self, df):
+        logging.info("Scaling and Labeling Data...")
+
+        features_scaled = df.drop(columns='Species')
+        scaled_df = StandardScaler().fit_transform(features_scaled)
+
+        self.encoder = LabelEncoder()
+        target = self.encoder.fit_transform(df['Species'])
+
+        # Save the fitted LabelBinarizer for later use
+        self.save_label_encoder()
+
+        return scaled_df, target
+    
+    def save_label_encoder(self):
+        with open('training/train_encoder.pkl', 'wb') as f:
+            pickle.dump(self.encoder, f)
+
+    def split_tensor(self, X, y):
+        logging.info("Splitting data into training and test sets...")
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,random_state=42)
+        
+        logging.info("Converting Sets into Tensors...")
+        X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+        X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+        y_train_tensor = torch.tensor(y_train, dtype=torch.long)
+        y_test_tensor = torch.tensor(y_test, dtype=torch.long)
+
+        return X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor
+    
+    def dataset_loader(self, X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor):
+        logging.info("Loading Data into Dataloader...")
+
+        train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+        test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+
+        train_loader = DataLoader(train_dataset, batch_size=conf['train']['batch_size'])
+        test_loader = DataLoader(test_dataset, batch_size=conf['train']['batch_size'])
+
+        return train_loader, test_loader
+
+
+class IrisNet(nn.Module):
+
+    def __init__(self) -> None:
+        super(IrisNet, self).__init__()
+
+        self.fc1 = nn.Linear(4, 16)
+        self.fc2 = nn.Linear(16, 16)
+        self.fc3 = nn.Linear(16, 3)
+    
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+    
 
 
 class Training():
+
     def __init__(self) -> None:
-        self.model = DecisionTreeClassifier(random_state=conf['general']['random_state'])
+        self.model = IrisNet()
 
-    def run_training(self, df: pd.DataFrame, out_path: str = None, test_size: float = 0.33) -> None:
+    def run_training(self, train_loader: DataLoader, test_loader: DataLoader, out_path: str = None, test_size: float = 0.33) -> None:
         logging.info("Running training...")
-        X_train, X_test, y_train, y_test = self.data_split(df, test_size=test_size)
         start_time = time.time()
-        self.train(X_train, y_train)
+
+        self.train(train_loader)
+
         end_time = time.time()
+
         logging.info(f"Training completed in {end_time - start_time} seconds.")
-        self.test(X_test, y_test)
+
+        self.test(test_loader)
         self.save(out_path)
-
-    def data_split(self, df: pd.DataFrame, test_size: float = 0.33) -> tuple:
-        logging.info("Splitting data into training and test sets...")
-        return train_test_split(df[['x1','x2']], df['y'], test_size=test_size, 
-                                random_state=conf['general']['random_state'])
     
-    def train(self, X_train: pd.DataFrame, y_train: pd.DataFrame) -> None:
+    def train(self, train_loader) -> None:
         logging.info("Training the model...")
-        self.model.fit(X_train, y_train)
+        
+        criterion = nn.CrossEntropyLoss()
 
-    def test(self, X_test: pd.DataFrame, y_test: pd.DataFrame) -> float:
+        optimizer = optim.Adam(self.model.parameters(), lr=0.01)
+
+        num_epochs = conf['train']['num_epochs']
+        for epoch in range(num_epochs):
+
+            for inp, labels in train_loader:
+                optimizer.zero_grad()
+
+                outputs = self.model.forward(inp)
+                loss = criterion(outputs, labels)
+
+                loss.backward()
+                optimizer.step()
+            
+            print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}')
+
+
+    def test(self, test_loader: DataLoader) -> float:
         logging.info("Testing the model...")
-        y_pred = self.model.predict(X_test)
-        res = f1_score(y_test, y_pred)
-        logging.info(f"f1_score: {res}")
-        return res
+        all_preds = []
+        all_labels = []
+
+        with torch.no_grad():
+            for inp, labels in test_loader:
+                outputs = self.model(inp)
+                _, predicted = torch.max(outputs.data, 1)
+                all_preds.extend(predicted.numpy())
+                all_labels.extend(labels.numpy())
+
+        with open('training/train_encoder.pkl', 'rb') as f:
+            encoder = pickle.load(f)
+
+        class_report = classification_report(all_labels, all_preds, target_names=encoder.classes_)
+
+        logging.info(f"Classification Report: {class_report}")
+        return class_report
 
     def save(self, path: str) -> None:
         logging.info("Saving the model...")
@@ -111,8 +202,7 @@ class Training():
         else:
             path = os.path.join(MODEL_DIR, path)
 
-        with open(path, 'wb') as f:
-            pickle.dump(self.model, f)
+        torch.save(self.model.state_dict(), path)
 
 
 def main():
@@ -121,8 +211,8 @@ def main():
     data_proc = DataProcessor()
     tr = Training()
 
-    df = data_proc.prepare_data(max_rows=conf['train']['data_sample'])
-    tr.run_training(df, test_size=conf['train']['test_size'])
+    train_loader, test_loader = data_proc.prepare_data()
+    tr.run_training(train_loader, test_loader)
 
 
 if __name__ == "__main__":
